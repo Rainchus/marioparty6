249c4c003c508368c073db4d803aa3636feba837
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-10 02:38:18 +0400

    Port dolphin/db from MP4 (matching)

--- configure.py
+++ configure.py
@@ -407,7 +407,7 @@ config.libs = [
     DolphinLib(
         "db",
         [
-            Object(NonMatching, "dolphin/db/db.c"),
+            Object(Matching, "dolphin/db/db.c"),
         ],
     ),
     DolphinLib(
