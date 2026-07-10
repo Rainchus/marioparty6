1d721176022eb0b54ee4a5bb155e17274e937107
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-10 05:08:09 +0400

    Port dolphin/dsp dsp_task from MP4 (matching)

--- configure.py
+++ configure.py
@@ -475,7 +475,7 @@ config.libs = [
         [
             Object(NonMatching, "dolphin/dsp/dsp.c"),
             Object(Matching, "dolphin/dsp/dsp_debug.c"),
-            Object(NonMatching, "dolphin/dsp/dsp_task.c"),
+            Object(Matching, "dolphin/dsp/dsp_task.c"),
         ],
     ),
     DolphinLib(
