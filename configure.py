ac5483b3833108eedce4be2ef6bce5074a62c767
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-10 16:18:28 +0400

    Port dolphin/gx GXFifo from MP4 (matching)

--- configure.py
+++ configure.py
@@ -482,7 +482,7 @@ config.libs = [
         "gx",
         [
             Object(NonMatching, "dolphin/gx/GXInit.c"),
-            Object(NonMatching, "dolphin/gx/GXFifo.c"),
+            Object(Matching, "dolphin/gx/GXFifo.c"),
             Object(NonMatching, "dolphin/gx/GXAttr.c"),
             Object(NonMatching, "dolphin/gx/GXMisc.c"),
             Object(Matching, "dolphin/gx/GXGeometry.c"),
