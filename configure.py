2998aa787e0fe2c20998c4e96f5d3f82c9a929ac
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-10 05:09:29 +0400

    Port dolphin/gx GXPerf from MP4 (matching)

--- configure.py
+++ configure.py
@@ -495,7 +495,7 @@ config.libs = [
             Object(NonMatching, "dolphin/gx/GXDraw.c"),
             Object(NonMatching, "dolphin/gx/GXDisplayList.c"),
             Object(NonMatching, "dolphin/gx/GXTransform.c"),
-            Object(NonMatching, "dolphin/gx/GXPerf.c"),
+            Object(Matching, "dolphin/gx/GXPerf.c"),
         ],
     ),
     DolphinLib(
