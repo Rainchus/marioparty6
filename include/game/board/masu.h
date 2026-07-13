c5c847e1d9ea08cb30730025248e0bf719a04dee
iirg4x <52885971+iirg4x@users.noreply.github.com>
2026-07-13 15:53:51 +0400

    feat(board): recover masu accessor core

--- include/game/board/masu.h
+++ include/game/board/masu.h
@@ -6,24 +6,60 @@
 #define MASU_LINK_MAX 5
 
 #define MASU_LAYER_DEFAULT 0
+#define MASU_LAYER_MAX 2
 
 #define MASU_FLAG_BATTAN 0x4000
 
 typedef struct Masu_s {
-    u8 _unk00[0x10];
+    u8 useMtxF : 1;
+    u8 _unk00_1 : 7;
+    u8 _unk01;
+    s16 _unk02;
+    HuVecF pos;
     u16 flag;
-    u8 _unk12[2];
+    u16 _unk12;
     u32 mAttr;
-    u8 _unk18[0x64 - 0x18];
+    HuVecF scale;
+    HuVecF rot;
+    u16 type;
+    s16 capsuleNo;
+    Mtx matrix;
     u16 linkNum;
     u16 linkTbl[MASU_LINK_MAX];
 } MASU;
 
-MASU *mbMasuGet(s16 id);
-u16 mbMasuAttrGet(s16 id);
+int mbMasuNumGet(void);
 int mbMasuRawNumGet(void);
-BOOL mbMasuPosGet(s16 id, HuVecF *pos);
+MASU *mbMasuGet(s16 id);
+void mbMasuLayerSet(int layer);
+int mbMasuLayerGet(void);
+u16 mbMasuAttrGet(s16 id);
+void mbMasuAttrSet(s16 id, u16 attr);
+u32 mbMasuMAttrGet(s16 id);
+void mbMasuMAttrSet(s16 id, u32 attr);
+u16 mbMasuTypeGet(s16 id);
+void mbMasuTypeSet(s16 id, int type);
+s16 mbMasuCapsuleGet(s16 id);
+void mbMasuPosGet(s16 id, HuVecF *pos);
+void mbMasuPosSet(s16 id, float x, float y, float z);
+void mbMasuPosSetV(s16 id, HuVecF *pos);
+void mbMasuRotGet(s16 id, HuVecF *rot);
+void mbMasuRotSet(s16 id, float x, float y, float z);
+void mbMasuRotSetV(s16 id, HuVecF *rot);
+void mbMasuMtxGet(s16 id, Mtx matrix);
+void mbMasuMtxSet(s16 id, Mtx matrix);
+BOOL mbMasuDispGet(void);
+void mbMasuDispSet(BOOL dispF);
+void mbMasuDispAttrSet(u16 attr);
+void mbMasuDispAttrReset(u16 attr);
+u16 mbMasuDispAttrGet(void);
+void mbMasuDispMAttrSet(u32 attr);
+void mbMasuDispMAttrReset(u32 attr);
+u32 mbMasuDispMAttrGet(void);
+s16 mbMasuLinkGet(s16 id, int linkNo);
+s16 mbMasuLinkNumGet(s16 id);
 int mbMasuLinkTblGet(s16 id, s16 *linkTbl);
+int mbMasuLinkTblGet2(s16 id, s16 *linkTbl, BOOL hookF);
 s16 mbMasuLinkParentGet(s16 id, s16 *linkTbl);
 int mbMasuFind_IdStepGet(s16 id, s16 targetId);
 int mbMasuFind_TypeStepGet2(s16 id, int type, BOOL arg2, BOOL arg3);
---
+++ src/board/masu.c
@@ -0,0 +1,209 @@
+#include "game/board/masu.h"
+#include "game/hsfex.h"
+#include "game/hu3d.h"
+
+static int masuLayer;
+static s16 masuNum[MASU_LAYER_MAX];
+static MASU *masuData[MASU_LAYER_MAX];
+static BOOL masuDispF;
+static u16 masuDispAttrMask[MASU_LAYER_MAX];
+static u32 masuDispMAttrMask[MASU_LAYER_MAX];
+
+int mbMasuNumGet(void)
+{
+    return masuNum[masuLayer] + 1;
+}
+
+int mbMasuRawNumGet(void)
+{
+    return masuNum[masuLayer];
+}
+
+MASU *mbMasuGet(s16 id)
+{
+    return &masuData[masuLayer][id];
+}
+
+void mbMasuLayerSet(int layer)
+{
+    masuLayer = layer;
+}
+
+int mbMasuLayerGet(void)
+{
+    return masuLayer;
+}
+
+u16 mbMasuAttrGet(s16 id)
+{
+    return masuData[masuLayer][id].flag;
+}
+
+void mbMasuAttrSet(s16 id, u16 attr)
+{
+    masuData[masuLayer][id].flag = attr;
+}
+
+u32 mbMasuMAttrGet(s16 id)
+{
+    return masuData[masuLayer][id].mAttr;
+}
+
+void mbMasuMAttrSet(s16 id, u32 attr)
+{
+    masuData[masuLayer][id].mAttr = attr;
+}
+
+u16 mbMasuTypeGet(s16 id)
+{
+    return masuData[masuLayer][id].type;
+}
+
+void mbMasuTypeSet(s16 id, int type)
+{
+    masuData[masuLayer][id].type = type;
+}
+
+s16 mbMasuCapsuleGet(s16 id)
+{
+    return masuData[masuLayer][id].capsuleNo;
+}
+
+void mbMasuPosGet(s16 id, HuVecF *pos)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    if (!masuP->useMtxF) {
+        *pos = masuP->pos;
+    } else {
+        pos->x = masuP->matrix[0][3];
+        pos->y = masuP->matrix[1][3];
+        pos->z = masuP->matrix[2][3];
+    }
+}
+
+void mbMasuPosSet(s16 id, float x, float y, float z)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    masuP->pos.x = x;
+    masuP->pos.y = y;
+    masuP->pos.z = z;
+}
+
+void mbMasuPosSetV(s16 id, HuVecF *pos)
+{
+    mbMasuPosSet(id, pos->x, pos->y, pos->z);
+}
+
+void mbMasuRotGet(s16 id, HuVecF *rot)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    if (masuP->useMtxF) {
+        Hu3DMtxRotGet(masuP->matrix, rot);
+    } else {
+        *rot = masuP->rot;
+    }
+}
+
+void mbMasuRotSet(s16 id, float x, float y, float z)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    masuP->rot.x = x;
+    masuP->rot.y = y;
+    masuP->rot.z = z;
+}
+
+void mbMasuRotSetV(s16 id, HuVecF *rot)
+{
+    mbMasuRotSet(id, rot->x, rot->y, rot->z);
+}
+
+void mbMasuMtxGet(s16 id, Mtx matrix)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    if (masuP->useMtxF) {
+        PSMTXCopy(masuP->matrix, matrix);
+    } else {
+        Mtx rot;
+        Mtx trans;
+
+        mtxRot(rot, masuP->rot.x, masuP->rot.y, masuP->rot.z);
+        PSMTXTrans(trans, masuP->pos.x, masuP->pos.y, masuP->pos.z);
+        PSMTXConcat(trans, rot, matrix);
+    }
+}
+
+void mbMasuMtxSet(s16 id, Mtx matrix)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    if (matrix != NULL) {
+        masuP->useMtxF = TRUE;
+        PSMTXCopy(matrix, masuP->matrix);
+    } else {
+        masuP->useMtxF = FALSE;
+    }
+}
+
+BOOL mbMasuDispGet(void)
+{
+    return masuDispF;
+}
+
+void mbMasuDispSet(BOOL dispF)
+{
+    masuDispF = dispF;
+}
+
+void mbMasuDispAttrSet(u16 attr)
+{
+    masuDispAttrMask[masuLayer] |= attr;
+}
+
+void mbMasuDispAttrReset(u16 attr)
+{
+    masuDispAttrMask[masuLayer] &= ~attr;
+}
+
+u16 mbMasuDispAttrGet(void)
+{
+    return masuDispAttrMask[masuLayer];
+}
+
+void mbMasuDispMAttrSet(u32 attr)
+{
+    masuDispMAttrMask[masuLayer] |= attr;
+}
+
+void mbMasuDispMAttrReset(u32 attr)
+{
+    masuDispMAttrMask[masuLayer] &= ~attr;
+}
+
+u32 mbMasuDispMAttrGet(void)
+{
+    return masuDispMAttrMask[masuLayer];
+}
+
+s16 mbMasuLinkGet(s16 id, int linkNo)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    return masuP->linkTbl[linkNo];
+}
+
+s16 mbMasuLinkNumGet(s16 id)
+{
+    MASU *masuP = &masuData[masuLayer][id];
+
+    return masuP->linkNum;
+}
+
+int mbMasuLinkTblGet(s16 id, s16 *linkTbl)
+{
+    return mbMasuLinkTblGet2(id, linkTbl, FALSE);
+}
